65c24c34846c7657ac44135fc2c0653b159fef23
dojyorin <ota_droid@live.jp>
2023-07-10 12:30:04 +0900

    merge to universal.

--- module/openai/src/universal.ts
+++ module/openai/src/universal.ts
@@ -1,18 +1,22 @@
 import {
     type CreateChatCompletionRequest,
     type CreateChatCompletionResponse,
+    type CreateImageRequest,
+    type ImagesResponse,
+    type ListModelsResponse,
+    type Model,
     fetchExtend
 } from "../deps.ts";
 
-type Convert<T> = Partial<Record<keyof T, unknown>>;
+type Opt<T> = Partial<Record<keyof T, unknown>>;
 
 interface AzureEndpoint{
     resource: string;
     version: string;
+    aad: boolean;
     deployment: {
         chat?: string;
     };
-    aad?: boolean;
 }
 
 export class OpenAI{
@@ -24,31 +28,59 @@ export class OpenAI{
         this.#azure = azure;
     }
 
-    async #fetch<T extends Convert<T>, U extends Convert<U>>(path:string, body?:T){
+    async #fetch<T extends Opt<T>, U extends Opt<U>>(path:string, body?:T){
         return <U>await fetchExtend(this.#azure ? `https://${this.#azure.resource}.openai.azure.com/openai/deployments/${path}` : `https://api.openai.com/v1/${path}`, "json", {
             method: body ? "POST" : "GET",
             body: body && JSON.stringify(body),
-            query: this.#azure && {
-                "api-version": this.#azure.version
-            },
             headers: {
-                ...(!this.#azure || this.#azure.aad) ? {
-                    "Authorization": `Bearer ${this.#key}`
-                } : {
+                ...(this.#azure && !this.#azure.aad) ? {
                     "api-key": this.#key
+                } : {
+                    "Authorization": `Bearer ${this.#key}`
                 },
                 ...body && {
                     "Content-Type": "application/json"
                 }
+            },
+            query: this.#azure && {
+                "api-version": this.#azure.version
             }
         });
     }
 
-    async createChatCompletion(option:CreateChatCompletionRequest):Promise<CreateChatCompletionResponse>{
-        if(this.#azure && !this.#azure.deployment.chat){
+    #azureUnsupported(){
+        if(this.#azure){
             throw new ReferenceError();
         }
+    }
 
-        return await this.#fetch(`/${this.#azure.deployment.chat}/chat/completions`, option);
+    #azureDeployment(deployment:keyof AzureEndpoint["deployment"]){
+        if(this.#azure && !this.#azure.deployment[deployment]){
+            throw new ReferenceError();
+        }
+    }
+
+    async createChatCompletion(option:CreateChatCompletionRequest):Promise<CreateChatCompletionResponse>{
+        this.#azureDeployment("chat");
+
+        return await this.#fetch(this.#azure ? `/${this.#azure.deployment.chat}/chat/completions` : "/chat/completions", option);
+    }
+
+    async createImage(option:CreateImageRequest):Promise<ImagesResponse>{
+        this.#azureUnsupported();
+
+        return await this.#fetch("/images/generations", option);
+    }
+
+    async listModels():Promise<ListModelsResponse>{
+        this.#azureUnsupported();
+
+        return await this.#fetch("/models");
+    }
+
+    async retrieveModel(model:string):Promise<Model>{
+        this.#azureUnsupported();
+
+        return await this.#fetch(`/models/${model}`);
     }
 }
