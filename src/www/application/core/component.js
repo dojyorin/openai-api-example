cd9d2186b9f758bb37461ffa1d5a96528e960972
dojyorin <ota_droid@live.jp>
2023-08-06 17:17:40 +0900

    replace.

--- src/www/application/core/component.js
+++
@@ -1,7 +0,0 @@
-import {fetchComponent, defineAsyncComponent} from "../deps.js";
-
-export default {
-    install(context){
-        context.component("g-x-reflect", defineAsyncComponent(fetchComponent(import.meta.resolve("./component/reflect.vue"))));
-    }
-};
--- src/www/application/core/provide.js
+++ src/www/application/core/provide.js
@@ -2,9 +2,8 @@ import {ref, reactive} from "../deps.js";
 
 export default {
     install(context){
-        context.provide("g-layout-navigation", ref(false));
         context.provide("g-layout-loading", ref(false));
         context.provide("g-layout-notify", reactive([]));
-        context.provide("g-increment-count", ref(0));
+        context.provide("g-thread-message", reactive([]));
     }
 };
