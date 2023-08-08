ae1444cbb17e5a0f6b9c13a7d392e25f4da4a1c0
dojyorin <ota_droid@live.jp>
2023-08-08 19:38:52 +0900

    remove old.

--- src/www/application/_core/provide.js
+++
@@ -1,9 +0,0 @@
-import {ref, reactive} from "../deps.js";
-
-export default {
-    install(context){
-        context.provide("g-layout-loading", ref(false));
-        context.provide("g-layout-notify", reactive([]));
-        context.provide("g-thread", reactive([]));
-    }
-};
