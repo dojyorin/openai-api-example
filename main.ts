4a1ed593d391f7346f34686805e0c3e9e5a727a1
dojyorin <ota_droid@live.jp>
2023-10-12 20:44:07 +0900

    newline.

--- main.ts
+++ main.ts
@@ -3,4 +3,6 @@ import {oak} from "./src/oak.ts";
 await Deno.serve({
     hostname: "0.0.0.0",
     port: 8765
-}, async request => await oak.handle(request) ?? new Response()).finished;
+}, async(request)=>{
+    return await oak.handle(request) ?? new Response();
+}).finished;
--- src/openai.ts
+++ src/openai.ts
@@ -2,10 +2,6 @@ import {OpenAI} from "../deps.ts";
 
 const key = Deno.env.get("OPENAI_API_KEY");
 
-if(!key){
-    throw new ReferenceError("Environment variable 'OPENAI_API_KEY' not found.");
-}
-
-const openai = new OpenAI(key, "v1");
+const openai = new OpenAI(key ?? "", "v1");
 
 export {openai};
