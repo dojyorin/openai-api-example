51c65547073dbda004d4189c09f8cce284836f9c
dojyorin <ota_droid@live.jp>
2023-10-31 16:40:32 +0900

    WIP: migrate.

--- src/www/application/sfc/layout/main_header.vue
+++ src/www/application/sfc/layout/main_header.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app-bar density="compact" class="text-white">
-        <v-app-bar-nav-icon @click="nav = !nav"></v-app-bar-nav-icon>
-        <v-app-bar-title class="text-h5">Application</v-app-bar-title>
+        <!-- <v-app-bar-nav-icon @click="nav = !nav"></v-app-bar-nav-icon> -->
+        <v-app-bar-title class="text-h5 ml-0 text-center">OpenAI API Example</v-app-bar-title>
 
         <template #image>
             <v-img gradient="45deg, #174AFF, #0ECDFF"></v-img>
@@ -10,13 +10,13 @@
 </template>
 
 <script>
-    import {defineComponent, inject} from "../../deps.js";
+    // import {defineComponent, inject} from "../../deps.js";
 
-    export default defineComponent({
-        setup(){
-            const nav = inject("@layout:navigation");
+    // export default defineComponent({
+    //     setup(){
+    //         const nav = inject("@layout:navigation");
 
-            return {nav};
-        }
-    });
+    //         return {nav};
+    //     }
+    // });
 </script>
--- src/www/application/sfc/provide.js
+++ src/www/application/sfc/provide.js
@@ -5,6 +5,5 @@ export const provide = {
         context.provide("@layout:navigation", ref(false));
         context.provide("@layout:loading", ref(false));
         context.provide("@layout:notify", reactive([]));
-        context.provide("@count", ref(0));
     }
 };
