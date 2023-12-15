1b1a74516e7a427f8eacb4c7efdd3ba044081b89
dojyorin <ota_droid@live.jp>
2023-12-15 18:54:56 +0900

    single file.

--- src/client/application/sfc/provide.js
+++
@@ -1,9 +0,0 @@
-import {ref, reactive} from "../deps.js";
-
-export const provide = {
-    install(context){
-        context.provide("@layout:navigation", ref(false));
-        context.provide("@layout:loading", ref(false));
-        context.provide("@layout:notify", reactive([]));
-    }
-};
