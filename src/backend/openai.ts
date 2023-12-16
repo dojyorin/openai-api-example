4397979450d14b74b3c4e1238c40a157eb61a0e2
dojyorin <ota_droid@live.jp>
2023-12-17 02:59:01 +0900

    WIP

--- src/backend/openai.ts
+++ src/backend/openai.ts
@@ -2,7 +2,7 @@ import {OpenAI, envGet} from "../../deps.ts";
 
 const oai = new OpenAI({
     fetch: fetch,
-    apiKey: envGet("OPENAI_KEY", "string", true)
+    apiKey: envGet("OPENAI__KEY", "string", true)
 });
 
 export {oai};
--- src/server/kv.ts
+++
@@ -1,4 +0,0 @@
-export const kv = await Deno.openKv();
-
-export const t_session = "session";
-export const t_user = "user";
