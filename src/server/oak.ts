c123db66296eeeb82eca761ebf92b24e8899d67b
dojyorin <ota_droid@live.jp>
2023-12-17 04:33:49 +0900

    fix route.

--- src/server/oak.ts
+++ src/server/oak.ts
@@ -1,11 +1,38 @@
-import {Oak} from "../../deps.ts";
+import {Oak, HttpError, mainPath} from "../../deps.ts";
 import {router} from "./routing/routing.ts";
-import {catchError, sendStatic} from "./mw/mod.ts";
 
 const oak = new Oak();
 
-oak.use(catchError());
-oak.use(sendStatic());
+oak.use(async({response}, next)=>{
+    try{
+        await next();
+    }
+    catch(e){
+        if(e instanceof HttpError){
+            response.status = e.status;
+            response.body = e.message;
+        }
+        else{
+            console.error(e);
+            response.status = 500;
+        }
+    }
+});
+
 oak.use(router.routes(), router.allowedMethods());
+
+oak.use(async(context, next)=>{
+    try{
+        await context.send({
+            root: `${mainPath()}/src/client`,
+            index: "index.html",
+            format: false
+        });
+    }
+    catch(e){
+        console.error(e);
+        await next();
+    }
+});
 
 export {oak};
