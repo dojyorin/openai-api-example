5f6449f05336a9ade4e586e3e01141e416e3cddb
Kazuki Ota <ota_droid@live.jp>
2023-12-19 11:26:25 +0900

    Fix route (#14)
    * fix route.
    
    * centered.
    
    * change var name.

--- deps.ts
+++ deps.ts
@@ -1,11 +1,3 @@
-export {
-    type Middleware,
-    type Request as OakRequest,
-    Application as Oak,
-    Router,
-    HttpError,
-    createHttpError
-} from "https://deno.land/x/oak@v12.6.1/mod.ts";
-
+export {type Request as OakRequest, Application as Oak, Router, HttpError, createHttpError} from "https://deno.land/x/oak@v12.6.1/mod.ts";
 export {default as OpenAI} from "https://deno.land/x/openai@v4.22.0/mod.ts";
 export {type Opt, mainPath, fetchExtend, envGet} from "https://deno.land/x/simple_utility@v1.5.0/mod.ts";
